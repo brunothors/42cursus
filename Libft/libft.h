831f5ea6d210a20548cd77c9e87ad33f4e40fd79
Bruno Nespoli Thorstensen <bnespoli@c2r6p5.42sp.org.br>
2023-10-18 19:46:30 -0300

    libft

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bnespoli <bnespoli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:59:21 by bnespoli          #+#    #+#             */
-/*   Updated: 2023/10/18 16:46:30 by bnespoli         ###   ########.fr       */
+/*   Updated: 2023/10/18 19:42:31 by bnespoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ int		ft_isprint(int i);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
